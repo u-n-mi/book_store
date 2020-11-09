9eb860c1a72f430502e8a1b47d2b9ee9ebf20b39
margarida santos <margarida.mimosa99@gmail.com>
2020-11-09 17:09:08 +0000

    user model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,5 @@
 class Order < ApplicationRecord
+    
+
+    validates :name, :address, :email, :presence => true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, presence: true
+  validates :photo, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true
+
+  has_many :roles
+  has_many :books, dependent: :destroy
+  has_many :orders, through: :books
+  has_attachment :photo
 end
